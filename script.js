092cdbbc1b899c5be50b6d660ea55329341bc0b4
moeenpatel <123477545+moeenpatel@users.noreply.github.com>
2023-09-13 22:03:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-let element = document.getElementById("colorSelect");
-
-function removeElement() {
-
-    let currentValue = element.value;
-    element.remove(currentValue);
-}
+const dropDown = document.querySelector("#colorSelect");
+const btn = document.getElementsByTagName("input")[0];
+btn.addEventListener("click", () => {
+  let option = document.getElementsByTagName("option");
+  for (let i = 0; i < option.length; i++)
+    if (option[i].innerText === dropDown.value)
+      return dropDown.removeChild(option[i]);
+});
